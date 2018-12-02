83bd17a7f22b68a396590724904acb307fd534b1
A00431152 <Bhagya.Shree@smu.ca>
2018-12-02 17:51:14 -0400

    pushing to branch login functionality

---
+++ ProjectNew/ProjectNew/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using ProjectNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectNew.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(ProjectNew.Models.UserAccount userModel)
+        {
+            using (Model1 db = new Model1())
+            {
+                UserAccount userDetails = db.UserAccounts.Where(x => x.Name == userModel.Name && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "wrong username or Password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["Id"] = userDetails.Id;
+                    Session["Name"] = userDetails.Name;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+        }
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["Id"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+
+}
--- ProjectNew/ProjectNew/Models/UserAccount.cs
+++ ProjectNew/ProjectNew/Models/UserAccount.cs
@@ -2,6 +2,7 @@ namespace ProjectNew.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -14,13 +15,24 @@ namespace ProjectNew.Models
         [Key]
         public int Id { get; set; }
 
+
+        [DisplayName("User Name")]
+        [Required(ErrorMessage = "This field is required")]
         [StringLength(50)]
         public string Name { get; set; }
 
         [StringLength(50)]
         public string Email { get; set; }
-
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required")]
         [StringLength(50)]
         public string Password { get; set; }
+
+        public string LoginErrorMessage
+        {
+            get;
+            set;
+        }
+
     }
 }
