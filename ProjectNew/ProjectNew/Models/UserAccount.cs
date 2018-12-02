8c39053ce726f8aaaed697876b9b36f529d2b12e
Sreeraj Punnoli <Sreeraj.Punnoli@smu.ca>
2018-12-02 00:57:18 -0400

    amusement park initial

---
+++ ProjectNew/ProjectNew/Models/UserAccount.cs
@@ -0,0 +1,26 @@
+namespace ProjectNew.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("UserAccount")]
+    public partial class UserAccount
+    {
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
