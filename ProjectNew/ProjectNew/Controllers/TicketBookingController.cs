3ae75234b8929f3205fe05df8bb5c62c7c28c99a
sreerajpunnoli <43034292+sreerajpunnoli@users.noreply.github.com>
2018-12-03 23:10:03 -0800

    PaymentPage

--- ProjectNew/ProjectNew/Controllers/TicketBookingController.cs
+++ ProjectNew/ProjectNew/Controllers/TicketBookingController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectNew.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,28 @@ namespace ProjectNew.Controllers
 {
     public class TicketBookingController : Controller
     {
+        Model3 db = new Model3();
+
         // GET: TicketBooking
-        public ActionResult Index()
+        public ActionResult Payment()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Payment([Bind(Include = "TicketDay,AvailableSeatCount")] TicketBooking ticketDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                //All transaction and email
+                db.TicketBookings.Add(ticketDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ticketDetail);
+        }
+
     }
 }
