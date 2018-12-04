9a58d323dabbfea6c33e6dbf2b97b67a00829cb3
Shahzan Magray <shahzan.magray@gmail.com>
2018-12-04 17:59:00 -0400

    Email Functionality
    added functionality. however when oayment work is being done add the parameters for email and booking ID and write a proper email in the body part to send an email

--- ProjectNew/ProjectNew/Controllers/TicketBookingController.cs
+++ ProjectNew/ProjectNew/Controllers/TicketBookingController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +14,46 @@ namespace ProjectNew.Controllers
         // GET: TicketBooking
         public ActionResult Index()
         {
+            
             return View();
+        }
+        
+        public JsonResult SendEmailToCust()
+        {
+            bool Result = false;
+            Result = SendEmail("shahzan.magray@gmail.com", "Hi", "<p>Hi Shahzan<br />Hi watsaup</p>");
+            return Json(Result, JsonRequestBehavior.AllowGet);
+        }
+        public bool SendEmail(string toEmail, string Subject, string Body)
+        {
+            try
+            {
+                string senderEmail = System.Configuration.ConfigurationManager.AppSettings["SenderEmail"].ToString();
+                string senderPassword = System.Configuration.ConfigurationManager.AppSettings["SenderPassword"].ToString();
+
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Timeout = 100000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+                MailMessage mm = new MailMessage(senderEmail, toEmail, Subject, Body);
+                mm.IsBodyHtml = true;
+                mm.BodyEncoding = UTF8Encoding.UTF8;
+                client.Send(mm);
+
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+
+                return false;
+            }
         }
     }
 }
