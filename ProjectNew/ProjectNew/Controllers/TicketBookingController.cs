8230f55b5600b83dbce4fdf4588aa70fa389c552
sreerajpunnoli <43034292+sreerajpunnoli@users.noreply.github.com>
2018-12-05 00:04:22 -0400

    conflict correction

--- ProjectNew/ProjectNew/Controllers/TicketBookingController.cs
+++ ProjectNew/ProjectNew/Controllers/TicketBookingController.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,6 +17,7 @@ namespace ProjectNew.Controllers
         // GET: TicketBooking
         public ActionResult Payment()
         {
+            
             return View();
         }
 
@@ -31,6 +35,43 @@ namespace ProjectNew.Controllers
 
             return View(ticketDetail);
         }
+        
+        public JsonResult SendEmailToCust()
+        {
+            bool Result = false;
+            Result = SendEmail("shahzan.magray@gmail.com", "Hi", "<p>Hi Shahzan<br />Hi watsaup</p>");
+            return Json(Result, JsonRequestBehavior.AllowGet);
+        }
+        public bool SendEmail(string toEmail, string Subject, string Body)
+        {
+            try
+            {
+                string senderEmail = System.Configuration.ConfigurationManager.AppSettings["SenderEmail"].ToString();
+                string senderPassword = System.Configuration.ConfigurationManager.AppSettings["SenderPassword"].ToString();
 
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Timeout = 100000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+                MailMessage mm = new MailMessage(senderEmail, toEmail, Subject, Body);
+                mm.IsBodyHtml = true;
+                mm.BodyEncoding = UTF8Encoding.UTF8;
+                client.Send(mm);
+
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+
+                return false;
+            }
+        }
     }
 }
